7c62ca8ff126c4baada72724f6bb2b6b181c02b3
Ali Rıza Çelik <riza-celik@outlook.com>
2021-02-06 17:40:53 +0300

    notedao
    notedao ekleme yapıldı

--- app/src/main/java/com/example/noteapp/data/db/dao/NotesDao.kt
+++ app/src/main/java/com/example/noteapp/data/db/dao/NotesDao.kt
@@ -5,13 +5,17 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.noteapp.data.db.models.Notes
+
 @Dao
 interface NotesDao {
     @Query("Select * from notes where userId=:userId")
-    fun getUserNotes(userId:Int):List<Notes>
+    fun getUserNotes(userId: Int): List<Notes>
 
     @Query("Select * from notes where noteId=:noteId")
-    fun getNoteInformation(noteId:Int):List<Notes>
+    fun getNoteInformation(noteId: Int): List<Notes>
+
+    @Query("Select * from notes where noteState=:noteState")
+    fun getNoteList(noteState: Int): List<Notes>
 
     @Insert
     fun insert(notes: Notes)
